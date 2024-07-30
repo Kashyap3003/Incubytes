b7548c1ae9dcab46709e07a4208f5c19299361a4
Kashyap3003 <kashyapajudiya11@gmail.com>
2024-07-30 17:33:22 +0530

    problem if seprater is not comma

--- string-calculator/tests/stringCalculator.test.js
+++ string-calculator/tests/stringCalculator.test.js
@@ -1,6 +1,13 @@
 const add = require('../src/stringCalculator');
 
-test('should return the number itself for a single number', () => {
-    expect(add("1")).toBe(1);
-    expect(add("5")).toBe(5);
+test('should return the sum of two numbers', () => {
+    expect(add("1,2")).toBe(3);
+    expect(add("10,20")).toBe(30);
+    expect(add("20,00")).toBe(20);
+});
+
+test('should return the sum of two numbers', () => {
+    expect(add("10/2")).toBe(12);
+    expect(add("10,20")).toBe(30);
+    expect(add("20,00")).toBe(20);
 });
